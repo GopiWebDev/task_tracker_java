25f60cf3ab820ef4003f2504c72b42e8845a6dfd
GopiWebDev <gopisv.webdev@gmail.com>
2025-02-21 00:30:49 +0530

    Add Task class with ID, description, status, and timestamps

--- src/Task.java
+++ src/Task.java
@@ -1,21 +1,38 @@
 import java.time.LocalDateTime;
 
 public class Task {
-//    id: A unique identifier for the task
-//    description: A short description of the task
-//    status: The status of the task (todo, in-progress, done)
-//    createdAt: The date and time when the task was created
-//    updatedAt: The date and time when the task was last updated
-
-    private int id;
-    private String description;
-    private TaskStatus status;
+    private static int counter = 1;
+    private final int id;
+    String description;
+    TaskStatus status;
     LocalDateTime createdAt;
     LocalDateTime updatedAt;
+
+    Task(String description){
+        this.id = counter++;
+        this.description = description;
+        this.status = TaskStatus.TODO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    int getId(){
+        return this.id;
+    }
+
+    void setStatus(TaskStatus status){
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Task ID: " + id + ", Description: " + description + ", Status: " + status + ", Created At: " + createdAt + ", Updated At: " + updatedAt;
+    }
 }
 
 enum TaskStatus {
-    PENDING,
+    TODO,
     IN_PROGRESS,
-    DONE
+    DONE;
 }
---
+++ src/TaskTracker.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class TaskTracker {
+    public static void main(String[] args){
+        Task task = new Task("Write Code");
+
+        System.out.println(task);
+    }
+}
