32c586ef05f05d76f6c6f4d773490040e0d0e444
GopiWebDev <gopisv.webdev@gmail.com>
2025-02-21 00:56:22 +0530

    feat: created task manager class with add and delete methods

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class TaskManager {
+    ArrayList<Task> tasks = new ArrayList<Task>();
+
+    void addTask(Task task){
+        this.tasks.add(task);
+    }
+
+    void deleteTask(int id){
+        this.tasks.removeIf(task -> task.getId() == id);
+    }
+
+    void getTaskList(){
+        for(Task task : this.tasks){
+            System.out.println(task);
+        }
+    }
 }
--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -1,9 +1,19 @@
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class TaskTracker {
     public static void main(String[] args){
-        Task task = new Task("Write Code");
 
-        System.out.println(task);
+        Task task1 = new Task("Task 1");
+        Task task2 = new Task("Task 2");
+        Task task3 = new Task("Task 3");
+        TaskManager manager = new TaskManager();
+        manager.addTask(task1);
+        manager.addTask(task2);
+        manager.addTask(task3);
+
+        manager.deleteTask(21);
+
+        manager.getTaskList();
     }
 }
